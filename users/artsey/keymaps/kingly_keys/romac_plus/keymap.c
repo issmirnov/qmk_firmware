bbea700541a3500f4f44064ee63082a722a62967
whydobearsxplod <whydobearsxplod@email.com>
2020-11-29 17:55:15 -0600

    Added oled code to romac.

--- users/artsey/keymaps/kingly_keys/romac_plus/keymap.c
+++ users/artsey/keymaps/kingly_keys/romac_plus/keymap.c
@@ -17,3 +17,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	
 };
 
+#ifdef OLED_DRIVER_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;  // flips the display 180 degrees if offhand
+}
+
+void oled_task_user(void) {
+  // Host Keyboard Layer Status
+  oled_write_P(PSTR("Let's\nbuild\nsome-\nthing\nto-\nget-\nher!"), false);
+  switch (get_highest_layer(layer_state)) {
+    case BASE:
+      oled_write_ln_P(PSTR(""), false);
+      break;
+    case FN:
+      oled_write_ln_P(PSTR("FN"), false);
+      break;
+    default:
+      // Or use the write_ln shortcut over adding '\n' to the end of your string
+      oled_write_ln_P(PSTR("Undef"), false);
+  }
+
+  // Host Keyboard LED Status
+  uint8_t led_usb_state = host_keyboard_leds();
+  oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK) ? PSTR("NLCK ") : PSTR("     "), false);
+  oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK) ? PSTR("CAPS ") : PSTR("       "), false);
+  oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLK") : PSTR("       "), false);
+}
+#endif
