2e23900ff289a1a595e1564bf5713b3be31f92ba
Ivan Smirnov <isgsmirnov@gmail.com>
2021-03-11 14:55:30 -0800

    use natiove artsey import

--- users/artsey/keymaps/keebio/levinson/rev2/keymap.c
+++ users/artsey/keymaps/keebio/levinson/rev2/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "quantum.h"
 #include "artsey.h"
-#include "g/keymap_combo.h"
+#include "keymap_combo.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
