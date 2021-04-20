37f3353747355b5e98c40ee7217d3fdbb962d8ac
Ivan Smirnov <ismirnov@stripe.com>
2021-04-20 11:19:07 -0700

    add artsey_left from https://github.com/artseyio/artsey-qmk

---
+++ keyboards/boardsource/3x4/keymaps/artsey_left/config.h
@@ -0,0 +1,18 @@
+#define COMBO_TERM 200
+
+// enable locking the one-shot mods by tapping them three times.
+#define ONESHOT_TAP_TOGGLE 3
+
+// To enable time-out for one-shot mods.
+#define ONESHOT_TIMEOUT 5000
+
+// improve the behavior of mousekeys.
+#define MOUSEKEY_INTERVAL 12
+#define MOUSEKEY_MAX_SPEED 6
+#define MOUSEKEY_TIME_TO_MAX 50
+#define MOUSEKEY_DELAY 20
+
+// improve the behavior of layer-tap keys.
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
